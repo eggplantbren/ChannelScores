3abc8ad7a2dcb3941c73be4c21201329cb6bb369
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-05-27 14:26:18 +1200

    Adding a channel

--- database.py
+++ database.py
@@ -17,24 +17,31 @@ def setup():
 CREATE TABLE IF NOT EXISTS channels
 (claim_hash           BLOB NOT NULL PRIMARY KEY,
  total_deweys         INTEGER NOT NULL,
- boost_factor         REAL NOT NULL DEFAULT 1.0,
+ boost_factor         REAL NOT NULL,
  boosted_total_deweys REAL NOT NULL)
 WITHOUT ROWID;""")
 
+    # Supports ALSO INCLUDES THE BID, which is treated as a self- or
+    # anonymous- support.
     db.execute("""
 CREATE TABLE IF NOT EXISTS supports
-(txo_hash        BLOB NOT NULL PRIMARY KEY,
- deweys          INTEGER NOT NULL,
+(deweys          INTEGER NOT NULL,
  signing_channel BLOB REFERENCES channels (claim_hash),
- boosted_deweys  REAL NOT NULL)
-WITHOUT ROWID;""")
+ boosted_deweys  REAL NOT NULL,
+ PRIMARY KEY (deweys, signing_channel));""")
 
     db.execute("COMMIT;")
 
 
+def add_channel(claim_hash, bid_deweys):
 
+    # Create the channel
+    db.execute("INSERT INTO channels VALUES (?, ?, ?, ?);",
+               (claim_hash, bid_deweys, 1.0, bid_deweys))
 
-
+    # Create the self-support
+    db.execute("INSERT INTO supports VALUES (?, ?, ?);",
+               (bid_deweys, claim_hash, bid_deweys))
 
 
 setup()
